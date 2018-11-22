59ea8c065360b453f2f36a6d02b98eabaf2a7629
Gopi Krishna <Gopiwayz@gmail.com>
2018-11-23 00:25:24 +0530

    some minor changes to accesibility

--- src/components/TimerControls.js
+++ src/components/TimerControls.js
@@ -4,10 +4,10 @@ const TimerControls = ({ handleTimer, handleReset, isPaused }) => {
   const playIcon = isPaused ?  <i className="fas fa-play" /> :  <i className="fas fa-pause" />
   return (
     <div className="timer-controls" >
-      <button id="start_stop" onClick={() => handleTimer()}>
-       
+      <button id="start_stop" onClick={() => handleTimer()} role="play and pause">
+       {playIcon}
       </button>
-      <button id="reset" onClick={() => handleReset()}>
+      <button id="reset" onClick={() => handleReset()} role="reset">
       <i className="fas fa-sync-alt"></i>
       </button>
     </div>
--- src/components/lengthContols.js
+++ src/components/lengthContols.js
@@ -4,11 +4,11 @@ const LengthContols = ({ type, length, increment, decrement }) => {
   return (
     <div className="length-controls">
       <div id={type + "-label"}>{type + " Length"}</div>
-      <button id={type + "-decrement"} onClick={() => decrement(type)}>
+      <button id={type + "-decrement"} onClick={() => decrement(type)} role={"Decrease " +type+" length"}>
         -
       </button>
       <div id={type + "-length"}>{length}</div>
-      <button id={type + "-increment"} onClick={() => increment(type)}>
+      <button id={type + "-increment"} onClick={() => increment(type)} role={"Increase " +type+" length"}>
         +
       </button>
     </div>
